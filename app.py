24073b47261861d70536961c611550d14e808902
pramodtechrabbit-png <pramodtechrabbit@gmail.com>
2026-02-13 13:20:49 +0530

    Refactor login function and clean up code
    Refactored login function and removed calculate_total function.

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
+def login(username, password):
+    if username == "admin" and password == "123":
+        return True
+    return False
+
 def divide(a, b):
     return a / b
 
-def calculate_total(price, tax):
-    total = price + tax
-    print("Total is:", total)
+print(divide(10, 0))
 
-def login(username, password):
-    if username == "admin" and password == "1234":
-        return True
-    else:
-        return False
-
-x = divide(10, 0)
-calculate_total(100, 18)
